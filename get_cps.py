24db7d98532e9b80e37f357514126c088d1dbadf
drex <allenjantony@gmail.com>
2018-06-30 16:16:12 +0530

    removed duplicates

--- get_cps.py
+++
@@ -1,7 +0,0 @@
-with open("text_files/selected_cps.txt",'w') as g:
-	with open("text_files/LT_cps.txt") as f:
-		for line in f.readlines() :
-			line = line.split(";")
-			first = line[0].split("|")[0].strip().lower()
-			last = line[1].split("|")[0].strip().lower()
-			g.write(first+" "+last+" \n")
